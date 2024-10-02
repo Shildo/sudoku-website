9a1ac9530bd3b54501706b0b5b30f112c296d485
Shildo <guido.marzocchini@gmail.com>
2024-10-02 11:36:42 -0300

    reorganize structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import styles from './page.module.scss';
-import Sudoku from './components/Sudoku/Sudoku';
+import Sudoku from './ui/components/Sudoku/Sudoku';
 
 async function getGrid() {
 	const res = fetch("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,difficulty,solution}}}")
@@ -11,8 +11,6 @@ export default async function Home() {
 	const grid = await getGrid();
 
 	const { solution, ...board } = grid.newboard.grids[0];
-
-	console.log(board);
 
 	return (
 		<main className={styles.main}>
